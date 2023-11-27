ab9a8c81e7bc68c1382a23373ecdfc3b69287c98
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-11-27 21:51:20 +0530

    Create Divisible_By_7.java

---
+++ Divisible_By_7.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Divisible_By_7 {
+    public static void main(String[] args) {
+        /*
+         * Given an n-digit large number in form of string, check whether it is
+         * divisible by 7 or not. Print 1 if divisible by 7, otherwise 0.
+         * 
+         * Input: num = "8955795758"
+         * Output: 1
+         * Explanation: 8955795758 is divisible
+         * by 7.
+         * 
+         */
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the string");
+        String num = sc.nextLine();
+
+        int rem = 0;
+        for(int i=0;i<num.length();i++)
+        {
+            int digit =Character.getNumericValue(num.charAt(i));
+            rem = (rem*10+digit)%7;
+        }
+
+        if(rem==0)
+        {
+            System.err.println("it is divisible");
+        }
+        else
+        {
+            System.out.println("not divisible");
+        }
+
+    }
+}
