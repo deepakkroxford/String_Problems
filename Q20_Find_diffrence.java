149d2fe3d8094206610cda8db8f55df1a495ff8a
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2024-02-13 22:52:16 +0530

    important

---
+++ Q20_Find_diffrence.java
@@ -0,0 +1,31 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        int total=0;
+        for(int i=0;i<t.length();i++)
+        {
+                char ch = t.charAt(i);
+               total = total + (int)ch;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+               char ch = s.charAt(i);
+            total =total - (int)ch;
+        }
+        
+        char ans = (char)total;
+        return ans;
+    }
+}
+
+
+public class Q20_Find_diffrence {
+   public static void main(String[] args) {
+    Solution ans = new Solution();
+
+    String s ="abcd";
+    String t ="abcde";
+
+    System.out.println(ans.findTheDifference(s,t));
+   } 
+}
