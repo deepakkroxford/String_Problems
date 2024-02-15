fb47f2966234751ee9b09fba9099b89de5cdd901
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2024-02-16 00:03:27 +0530

    important

---
+++ Q22_reverse_vowel.java
@@ -0,0 +1,64 @@
+/**
+ * solution
+ */
+class solution {
+    boolean isvowel(char ch) {
+        if (ch == 'a' || ch == 'A') {
+            return true;
+        }
+        if (ch == 'e' || ch == 'E') {
+            return true;
+        }
+        if (ch == 'i' || ch == 'I') {
+            return true;
+        }
+        if (ch == 'o' || ch == 'O') {
+            return true;
+        }
+        if (ch == 'u' || ch == 'U') {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
+
+public class Q22_reverse_vowel {
+    public static void main(String[] args) {
+        solution ans = new solution();
+        String s="leetcode";
+        char arr[] = s.toCharArray();
+
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+    
+          if (ans.isvowel(arr[i]) && ans.isvowel(arr[j])) {
+           char temp=arr[i];
+           arr[i]=arr[j];
+           arr[j]=temp;
+    
+            i++;
+            j--;
+          }
+    
+          else if (ans.isvowel(arr[i])) {
+            j--;
+          } else if (ans.isvowel(arr[j])) {
+            i++;
+          } else {
+              //this else check if both are not string then it i++,j--;
+            i++;
+            j--;
+          }
+    }
+
+    for(int x =0;x<arr.length;x++)
+    {
+        System.out.print(arr[x]+" ");
+    }
+}
+}
+
---
+++ Q23_find_first_palindromic_string_in_array.java
@@ -0,0 +1,86 @@
+/**
+ *
+ */
+class solution {
+    boolean isplaindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+}
+
+public class Q23_find_first_palindromic_string_in_array {
+    public static void main(String[] args) {
+
+        /*
+         * 
+         * 2108. Find First Palindromic String in the Array
+         * 
+         * Given an array of strings words, return the first palindromic string in the
+         * array. If there is no such string, return an empty string "".
+         * 
+         * A string is palindromic if it reads the same forward and backward.
+         * 
+         * Example 1:
+         * Input: words = ["abc","car","ada","racecar","cool"]
+         * Output: "ada"
+         * Explanation: The first string that is palindromic is "ada".
+         * Note that "racecar" is also palindromic, but it is not the first.
+         * 
+         * Example 2:
+         * Input: words = ["notapalindrome","racecar"]
+         * Output: "racecar"
+         * Explanation: The first and only string that is palindromic is "racecar".
+         * 
+         * 
+         * Example 3:
+         * Input: words = ["def","ghi"]
+         * Output: ""
+         * Explanation: There are no palindromic strings, so the empty string is
+         * returned.
+         * 
+         */
+        solution ans = new solution();
+        String[] words = { "abc", "car", "ada", "racecar", "cool" };
+        for (int i = 0; i < words.length; i++) {
+            String s = words[i];
+            if (ans.isplaindrome(s)) {
+                System.out.println("the first palindromic string is ===>>  " + s);
+                break;
+            }
+        }
+
+        /*
+         * second way is
+         * 
+         * for(int i = 0; i < words.length; i++)
+         * {
+         * String s = words[i];
+         * int x=0;
+         * int y=s.length()-1;
+         * while(x<=y)
+         * {
+         * if(s.charAt(x)!=s.charAt(y))
+         * {
+         * break;
+         * }
+         * x++;
+         * y--;
+         * }
+         * if(x>y)
+         * {
+         * System.out.println(s);
+         * break;
+         * }
+         * }
+         */
+    }
+}
